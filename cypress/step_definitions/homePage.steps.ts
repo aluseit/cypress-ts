049414067f90efb214c2659598f2ff31efb9f532
aluseit <seytkalieva.alua@gmail.com>
2024-06-14 11:39:26 +0200

    updated step definitions

--- cypress/step_definitions/homePage.steps.ts
+++ cypress/step_definitions/homePage.steps.ts
@@ -2,16 +2,17 @@ import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor'
 
 Given('I am on the home page', () => {
   // Implement your code to navigate to the home page
-  cy.visit('http://example.com/home');
+  cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 });
 
 When('I type and submit in the board name', () => {
   // Implement your code to type and submit the board name
-  cy.get('input[name="boardName"]').type('New Board');
+  cy.get('input[name="username"]').type('Admin');
+  cy.get('input[name="password"]').type('admin123');
   cy.get('button[type="submit"]').click();
 });
 
-Then('I should see the new board', () => {
+Then('I should be redirected to the board detail', () => {
   // Implement your code to verify the new board is visible
-  cy.contains('New Board').should('be.visible');
+  cy.url().should('contains', 'dashboard');
 });
